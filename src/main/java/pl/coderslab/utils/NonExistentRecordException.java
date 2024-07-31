449c8ef90f379a40d56e6d1cc5549ae44b50bfad
Wojciech-Stanicki <stanickiw@gmail.com>
2024-07-31 23:36:25 +0200

    refactor: delete hardcoded messages in custom NonExistentRecordException

--- src/main/java/pl/coderslab/utils/NonExistentRecordException.java
+++ src/main/java/pl/coderslab/utils/NonExistentRecordException.java
@@ -1,20 +1,20 @@
 package pl.coderslab.utils;
 
-public class NonExistentRecord extends Exception {
+public class NonExistentRecordException extends Exception {
 
-    public NonExistentRecord(String errorMessage, Throwable cause) {
-        super("Specified record does not exist: " + errorMessage, cause);
+    public NonExistentRecordException(String errorMessage, Throwable cause) {
+        super(errorMessage, cause);
     }
 
-    public NonExistentRecord(String errorMessage) {
-        super("Specified record does not exist: " + errorMessage);
+    public NonExistentRecordException(String errorMessage) {
+        super(errorMessage);
     }
 
-    public NonExistentRecord(Throwable cause) {
+    public NonExistentRecordException(Throwable cause) {
         super(cause);
     }
 
-    public NonExistentRecord() {
+    public NonExistentRecordException() {
         super();
     }
 }
